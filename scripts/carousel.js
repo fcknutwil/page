ef49840fbb424ad963ca50f303d82c7cf564b5f9
maesi <mail@maesi.org>
2019-03-27 22:27:27 +0100

    Stand Absprache mit Raphi

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,4 +1,30 @@
+$( document ).ready(function() {
+    setContent(1);
+    var odd = true;
+    setInterval(function() {
+        console.log('bilderwechsel');
+        if(odd) {
+            $('#bilderwechsel img').attr('src', 'images/bilderwechsel2.png');
+        } else {
+            $('#bilderwechsel img').attr('src', 'images/bilderwechsel.png');
+        }
+        odd ^= true;
+    }, 5000);
+});
+
 function changeTab(index) {
     $('#carousel .active').removeClass('active');
     $('#carousel .tab:nth-child('+index+')').addClass('active');
+    setContent(index);
 }
+
+function setContent(index) {
+    $('#carousel .content').empty();
+    $('#platzhalter .carousel_content_'+index).children().clone().appendTo('#carousel .content');
+}
+
+function newsToContent() {
+    $('#bilderwechsel').empty();
+    $('#platzhalter .news').children().clone().appendTo('#bilderwechsel');
+}
+
