22db6f60a10e083aa7ee424817ebf5be13aad886
(U116399) <marcel.arnold@mobi.ch>
2019-04-17 19:06:34 +0200

    Page-Komponente hinzugefügt

--- src/app/mannschaften/mannschaften.module.ts
+++ src/app/mannschaften/mannschaften.module.ts
@@ -4,10 +4,12 @@ import { MannschaftenRoutingModule } from './mannschaften-routing.module';
 import { MannschaftComponent } from './mannschaft/mannschaft.component';
 import { MannschaftenComponent } from './mannschaften/mannschaften.component';
 import { SpielberichteComponent } from './spielberichte/spielberichte.component';
+import { PageModule } from '../page/page.module';
 
 @NgModule({
   declarations: [MannschaftenComponent, MannschaftComponent, SpielberichteComponent],
   imports: [
+    PageModule,
     CommonModule,
     MannschaftenRoutingModule
   ]
